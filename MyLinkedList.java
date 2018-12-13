89a785a95ca9d9d3d0a4cab596c8f1bb5136c958
stefansor <ssorobay00@stuy.edu>
2018-12-12 22:08:30 -0500

    bare minimum

---
+++ MyLinkedList.java
@@ -0,0 +1,13 @@
+class Node{
+ private int data;
+ private Node next,prev;
+}
+
+class MyLinkedList{
+ private int size;
+ private Node start,end;
+
+ public int size();
+ public boolean add(int value);
+ public String toString();
+}
