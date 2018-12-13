11b8d5d5d2feee3c43d0801b395a9653247f096e
stefansor <ssorobay00@stuy.edu>
2018-12-12 22:12:17 -0500

    constructors

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,6 +1,19 @@
 class Node{
  private int data;
  private Node next,prev;
+ public Node(int num){
+   data = num;
+ }
+ public Node(int num, Node first){
+   data = num;
+   next = first;
+ }
+ public Node(int num, Node first, Node second){
+   data = num;
+   next = first;
+   prev = second;
+ }
+ 
 }
 
 class MyLinkedList{
