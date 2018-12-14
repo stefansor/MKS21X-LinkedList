1ed918c5a36a6dae17b72c990446febb2bee9c6e
stefansor <ssorobay00@stuy.edu>
2018-12-14 11:03:41 -0500

    all hope is lsot

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -6,10 +6,30 @@ class Node{
    next = null;
    prev = null;
  }
- public Node(int num, Node first, Node second){
+ public Node(Integer num, Node first, Node second){
    data = num;
    next = first;
    prev = second;
+ }
+ public Node next(){
+   return this.next;
+ }
+ public Node setNext(Integer newnum){
+   Integer olnum = this.next.data;
+   this.next.prev = null;
+   this.next = Node(newnum);
+   this.next.prev = this;
+   return olnum;
+ }
+ public Node prev(){
+   return this.prev;
+ }
+ public Node setPrev(Integer newnum){
+   Integer olnum = this.prev.data;
+   this.prev.next = null;
+   this.prev = Node(newnum);
+   this.prev.next = this;
+   return olnum;
  }
 
 }
@@ -23,17 +43,47 @@ class MyLinkedList{
    start = null;
    end = null;
  }
+
  public boolean add(Integer value){
    if(end == null && start == null){
-     end = Node(value);
+     Node(value, null, null);
      length = 1;
+     return truep;
    }
    else{
-     this.end.next = Node(value);
+     this.end.setNext(Node(value, null, this));
      this.end = this.end.next;
      length++;
+     return true;
    }
+   return false;
  }
- public int size();
- public String toString();
+
+ public int size(){
+   return this.length;
+ }
+ public String toString(){
+  String str = "[";
+  Node current = this.start;
+  for(int i = 0; i < this.size(); i++){
+    if(i == this.size() - 1){
+      str = str + current.data + "]";
+    }
+    else{
+     str = str + current.data + ", ";
+     current = current.next;
+   }
+  }
+  return str;
+ }
+
+
+}
+
+public class Driver{
+ public static void main(String[] args){
+   MyLinkedList lst = new MyLinkedList();
+   lst.add(9);
+   System.out.println(lst);
+ }
 }
