28a79d6179c2bd45d221b1178378c600f1b25fb1
stefansor <ssorobay00@stuy.edu>
2018-12-16 14:46:25 -0500

    finished adding all methods must work out kink in remove value

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -15,25 +15,33 @@ class Node{
    return this.next;
  }
  public Node setNext(Node newnd){
-   if(this.next == null){
-     this.next = newnd;//changes next node in node
-     this.next.prev = this;
+   if(newnd == null){
+     this.next = null;
      return null;
    }
+   if(this.next == null){
+     this.next = newnd;//changes next node in node
+     newnd.prev = this;
+     return newnd;
+   }
    Node olnum = this.next;
-   this.next.prev = null;
+   olnum.prev = null;
    this.next = newnd;
-   this.next.prev = this;
+   newnd.prev = this;
    return olnum;
  }
  public Node prev(){
    return this.prev;
  }
  public Node setPrev(Node newnum){
+   if(newnum == null){
+     this.prev = null;
+     return null;
+   }
    if(this.prev == null){
      this.prev = newnum;//changes prev node in node
-     this.prev.next = this;
-     return null;
+     newnum.next = this;
+     return newnum;
    }
    Node olnum = this.prev;
    this.prev.next = null;
@@ -52,8 +60,8 @@ class Node{
    return ol;
  }
  public String toString(){
-   String str = "" + this.data;
-   return str;
+   String str = "" + this.getData();// + "    next: " + this.next().getData() + "    prev: " + this.prev().getData();
+   return str;//debugging for setnext and setprev
  }
 
 }
@@ -90,6 +98,9 @@ class MyLinkedList{
  }
  public String toString(){
   String str = "[";
+  if(this.length == 0){
+    return "[]";
+  }
   Node current = this.start;
   for(int i = 0; i < this.size(); i++){
     if(i == this.size() - 1){
@@ -106,7 +117,7 @@ class MyLinkedList{
  private Node getnthNode(int index){
    int cur = 0;//index of current
    Node current = this.start;//current node
-   while(cur != index){
+   while(cur != index && cur < this.size()){
      current = current.next();
      cur++;
    }
@@ -125,37 +136,38 @@ class MyLinkedList{
 
 public boolean contains(Integer value){
   boolean in = false;
-  int cur = 0;
+  int cur = 0;//keeps track of index
   Node current = this.start;
   while(cur < this.size()){
     if(current.getData() == value){
-      in = true;
+      in = true;//if the value is found in becomes true
     }
     current = current.next();
-    cur++;
+    cur++;//index and next progress to the next node
   }
   return in;
 }
 
 public int indexOf(Integer value){
-  int cur = 0;
-  Node current = this.start;
+  int cur = 0;//keeps track of index
+  Node current = this.start;//current node
   while(cur < this.size()){
     if(current.getData() == value){
-      return cur;
+      return cur;//if a node is founs with value then return index
     }
     current = current.next();
-    cur++;
+    cur++;//otherwise progress down the linked list
   }
-  return -1;
+  return -1;//while loop is done with no return - return -1 to show value isnt in
 }
 
 public void add(int index, Integer value){
-  Node addnod = new Node(value);
-  if(index == 0){
+  Node addnod = new Node(value);//new node to add with value
+  if(index == 0){//start case is unique to any other case
     this.start.setPrev(addnod);
     addnod.setNext(this.start);
     this.start = addnod;
+    this.length++;
   }
   else{
     Node curprev = this.getnthNode(index - 1);
@@ -164,31 +176,213 @@ public void add(int index, Integer value){
     curnext.setPrev(addnod);
     addnod.setPrev(curprev);
     addnod.setNext(curnext);
+    this.length++;
   }
 }
+
+
+public Integer remove(int index){
+  Node removee = this.getnthNode(index);
+  if(index == 0){
+    Integer ol = this.start.getData();
+    this.start = this.start.next();
+    this.start.setPrev(null);
+    this.length--;
+    return ol;
+  }
+  if(index == this.size() - 1){
+    Integer ol = this.end.getData();
+    this.end = this.end.prev();
+    this.end.setNext(null);
+    this.length--;
+    return ol;
+  }
+  else{
+    Integer ol = this.getnthNode(index).getData();
+    Node preremovee = this.getnthNode(index - 1);
+    Node postremovee = this.getnthNode(index + 1);
+    preremovee.setNext(postremovee);
+    postremovee.setPrev(preremovee);
+    removee.setNext(null);
+    removee.setPrev(null);
+    this.length--;
+    return ol;
+  }
+}
+
+public boolean remove(Integer value){
+  if(this.contains(value)){
+    this.remove(this.indexOf(value));
+    return true;
+  }
+  return false;
+}
+
 
 
 }
 
 class Driver{
- public static void main(String[] args){
-   Node first = new Node(9, null, null);
-   System.out.println(first );
-   MyLinkedList lst = new MyLinkedList();
-   lst.add(9);
-   lst.add(10);
-   System.out.println(lst);
-   System.out.println(lst.get(1));
-   System.out.println(lst.set(1, 4));
-   System.out.println(lst.get(1));
-   System.out.println(lst.size());
-   System.out.println(lst.contains(4));
-   System.out.println(lst.contains(10));
-   System.out.println(lst.indexOf(4));
-   System.out.println(lst.indexOf(10));
-   System.out.println(lst);
-   lst.add(1, 19);
-   System.out.println(lst);
-   System.out.println(lst.size());
- }
+  public static void main(String[] args) {
+      //What's poppin' everybody, welcome to my driver, we chill here in the code house
+      //Let's test this class and get this bread
+
+      //Step One: Creating a valid Instance
+      System.out.println("######################");
+      System.out.println("##New Instance Test:##");
+      System.out.println("######################");
+      MyLinkedList list = new MyLinkedList();
+      System.out.println(list);
+      System.out.println("This should print: \"[]\"\n");
+
+      //Step Two: Let's add some values
+      System.out.println("#######################");
+      System.out.println("##Adding Values Test:##");
+      System.out.println("#######################");
+      for(int i = 0; i < 10; i++) {
+          list.add(i);
+          //System.out.println(list.size()); <- Debugging
+      }
+      System.out.println(list);
+      System.out.println("This should print: \"[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\"");
+
+      //Step Three: Adding a value to the middle
+      System.out.println("#####################################");
+      System.out.println("##Adding Values in the Middle Test:##");
+      System.out.println("#####################################");
+      list.add(5, 999);
+      System.out.println(list);
+      System.out.println("This should print: \"[0, 1, 2, 3, 4, 999, 5, 6, 7, 8, 9]\"");
+
+      //Step Four: Removing a value from the list by index
+      System.out.println("##########################");
+      System.out.println("##Removing a Value Test:##");
+      System.out.println("##########################");
+      list.remove(5);
+      System.out.println(list);
+      System.out.println("This should print: \"[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\"");
+
+      //Step Five: Remove a value from the list by value
+      System.out.println("#####################################");
+      System.out.println("##Remove a Value by its Value Test:##");
+      System.out.println("#####################################");
+      list.remove(new Integer(9));
+      System.out.println(list);
+      System.out.println("This should print: \"[0, 1, 2, 3, 4, 5, 6, 7, 8]\"");
+
+      //Step Six: Testing the Contains Method
+      System.out.println("##############################");
+      System.out.println("##Checking Containment Test:##");
+      System.out.println("##############################");
+      System.out.println(list.contains(new Integer(999)));
+      System.out.println("This should print: false");
+
+      //Step Seven: Testing the Size Method
+      System.out.println("##############");
+      System.out.println("##Size Test:##");
+      System.out.println("##############");
+      System.out.println(list.size());
+      System.out.println("This should print: 9");
+
+      //Step Eight: indexOf Method
+      System.out.println("########################");
+      System.out.println("##Index of Value Test:##");
+      System.out.println("########################");
+      System.out.println(list.indexOf(0));
+      System.out.println("This should print: 0");
+
+      //Step Nine: Exception Testing
+      System.out.println("######################");
+      System.out.println("##Exception Testing:##");
+      System.out.println("######################");
+      try {
+          list.remove(-1);
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Removing a negative index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.remove(9000);
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Removing an oversized index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.add(-1, new Integer(9));
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Adding to a negative index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.add(90000, new Integer(69));
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Adding to an oversized index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.set(-1, new Integer(9));
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Setting a negative index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.set(90000, new Integer(69));
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Setting an oversized index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.get(-1);
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Getting a negative index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.get(90000);
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Getting an oversized index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+
+      //Step 10: Edge Case Testing
+      System.out.println("######################");
+      System.out.println("##Edge-Case Testing:##");
+      System.out.println("######################\n");
+
+      System.out.println("Removing Last Element:");
+      list.remove(8);
+      System.out.println(list);
+      System.out.println("This should print: [0, 1, 2, 3, 4, 5, 6, 7]\n");
+
+      System.out.println("Removing First Element:");
+      list.remove(0);
+      System.out.println(list);
+      System.out.println("This should print: [1, 2, 3, 4, 5, 6, 7]\n");
+
+      System.out.println("Adding to Last Element:");
+      list.add(7, new Integer(8));
+      System.out.println(list);
+      System.out.println("This should print: [1, 2, 3, 4, 5, 6, 7, 8]\n");
+
+      System.out.println("Adding to First Element:");
+      list.add(0, new Integer(666));
+      System.out.println(list);
+      System.out.println("This should print: [666, 1, 2, 3, 4, 5, 6, 7, 8]\n");
+  }
 }
